285c983a9132fc432367a475437dafc437e14612
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-06-01 15:22:29 -0500

    Initial commit

---
+++ src/test/java/com/pawsomeadoptions/capstoneproject/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawsomeadoptions.capstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
