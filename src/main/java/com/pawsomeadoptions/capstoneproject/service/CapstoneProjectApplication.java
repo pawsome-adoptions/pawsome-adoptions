5369bc4143f1aa60d079d92b81053fce2719527c
Daniela Salas <danielapiconsalas@gmail.com>
2023-06-01 15:40:38 -0500

    added packages and commented out security

--- src/main/java/com/pawsomeadoptions/capstoneproject/service/CapstoneProjectApplication.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/service/CapstoneProjectApplication.java
@@ -1,4 +1,4 @@
-package com.pawsomeadoptions.capstoneproject;
+package com.pawsomeadoptions.capstoneproject.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
