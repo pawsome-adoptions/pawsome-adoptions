a5f97653a78e5bc4f72b5cccb8056cc7437ba402
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-06-09 11:12:26 -0500

    edit working

--- src/main/java/com/pawsomeadoptions/capstoneproject/SecurityConfiguration.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration {
                 .requestMatchers(
                         "/userpost",
                         "/userpost/{postID}",
-                        "/profile"
+                        "/profile","/userpost/{id}/edit", "/posts/submitEdit"
                 ) // only authenticated users can create/edit ads
                 .authenticated()
                 /* Pages that require authentication */
--- src/main/java/com/pawsomeadoptions/capstoneproject/controllers/PostsController.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/controllers/PostsController.java
@@ -57,4 +57,46 @@ public class PostsController {
         model.addAttribute("post", post);
         return "posts/visitor-post";
     }
+
+    //post edit for users logged in
+    @GetMapping("/userpost/{id}/edit")
+    public String editUsersSinglePost(@PathVariable Long id, Model model){
+        Post postToEdit = postDao.getReferenceById(id);
+
+        model.addAttribute("postToViewLayer", postToEdit);
+        postDao.save(postToEdit);
+        return "posts/edit-post";
+    }
+
+    @PostMapping("/posts/submitEdit")
+    public String submitPostEdit(@ModelAttribute Post post){
+        post.setCategory("dog");
+        post.setImg("urlHere");
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        user = userDao.getReferenceById(user.getId());
+        post.setUsers(user);
+        postDao.save(post);
+        return "redirect:/profile";
+    }
+
+//    @PostMapping("/userpost/{id}/edit")
+//    public String editUsersSinglePost(@ModelAttribute("postToViewLayer") Post post){
+//        Post postToEdit = postDao.getReferenceById(id);
+//
+////        model.addAttribute("postToViewLayer", postToEdit);
+//        postDao.save(postToEdit);
+//        return "posts/edit-post";
+//    }
+
+
+//    @PostMapping("/profile")
+//    public String editPost(@RequestParam(name = "title") String title,
+//                              @RequestParam(name = "description") String description, @RequestParam(name = "postImg") String postImg){
+//        Post post = new Post();
+//        post.setTitle(title);
+//        post.setDescription(description);
+//        post.getImg();
+//        postDao.save(post);
+//        return "redirect:/profile";
+//    }
 }
