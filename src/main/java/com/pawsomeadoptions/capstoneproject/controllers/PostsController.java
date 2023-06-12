a14154dad889d42f9355e0fe3c12f001e950c2b7
Daniela Salas <danielapiconsalas@gmail.com>
2023-06-12 16:14:41 -0500

    commiting

--- src/main/java/com/pawsomeadoptions/capstoneproject/controllers/PostsController.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/controllers/PostsController.java
@@ -21,22 +21,23 @@ public class PostsController {
         this.userDao = userDao;
     }
 
-//    posts for visitors who aren't logged in
+    //    posts for visitors who aren't logged in
     @GetMapping("/visitorpost")
-    public String showVisitorsPosts() {
-            return "posts/visitor-post";
+    public String showVisitorsPosts(Model model) {
+        model.addAttribute("posts", postDao.findAll());
+        return "posts/visitor-post";
     }
 
     //signle posts for visitors who aren't logged in
-    @GetMapping("/visitorpost/{postID}")
+    @GetMapping("/visitorsinglepost")
     public String showSingleVisitorPosts() {
-
         return "posts/single-visitor";
     }
 
     //posts for users who are logged in
     @GetMapping("/userpost")
     public String userPostForm(Model model) {
+        model.addAttribute("posts", postDao.findAll());
         model.addAttribute("post", new Post());
         return "posts/user-post";
     }
@@ -57,4 +58,37 @@ public class PostsController {
         model.addAttribute("post", post);
         return "posts/visitor-post";
     }
+
+    //post edit for users logged in
+    @GetMapping("/userpost/{id}/edit")
+    public String editUsersSinglePost(@PathVariable Long id, Model model) {
+        Post postToEdit = postDao.getReferenceById(id);
+
+        model.addAttribute("postToViewLayer", postToEdit);
+        postDao.save(postToEdit);
+        return "posts/edit-post";
+    }
+
+    @PostMapping("/posts/submitEdit")
+    public String submitPostEdit(@ModelAttribute Post post) {
+
+        //Note: we are hardcoding here, need to have this functionality implemented
+//        post.setImg("urlHere");
+
+        //This code is good to go
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        user = userDao.getReferenceById(user.getId());
+        post.setUsers(user);
+        postDao.save(post);
+        return "redirect:/profile";
+    }
+
+    @GetMapping("/userpost/{id}/delete")
+    public String deletePost(@PathVariable Long id){
+        Post postToDelete = postDao.getReferenceById(id);
+        postDao.delete(postToDelete);
+
+        return "redirect:/profile";
+    }
+
 }
--- src/main/java/com/pawsomeadoptions/capstoneproject/repositories/PostRepository.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/repositories/PostRepository.java
@@ -12,4 +12,6 @@ public interface PostRepository extends JpaRepository<Post, Long>{
 
     List<Post> findAllByUser(User user);
 
+    List<Post> findByCategory(String category);
+
 }
