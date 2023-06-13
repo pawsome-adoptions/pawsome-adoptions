5c5bbec7077cf79f8c8b1a3ba26d535b2d0f5da2
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-06-13 15:31:54 -0500

    Error pages fully functionable, need a little more style tlc

--- src/main/java/com/pawsomeadoptions/capstoneproject/SecurityConfiguration.java
+++ src/main/java/com/pawsomeadoptions/capstoneproject/SecurityConfiguration.java
@@ -55,7 +55,7 @@ public class SecurityConfiguration {
                 /* Pages that require authentication */
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/visitorsinglepost", "/home","/userpost", "/deleteUser", "/visitorpost", "/visitorpost/{id}", "/sign-up", "/adopt", "/adopt/{id}","/reset-password", "/about", "/invalidUsernameOrPassword", "/js/**", "/css/**", "/img/**", "/static/**", "/resetpassword") // anyone can see home, the ads pages, and sign up
+                .requestMatchers("/visitorsinglepost", "/home","/userpost", "/deleteUser", "/visitorpost", "/visitorpost/{id}", "/sign-up", "/adopt", "/adopt/{id}","/reset-password", "/about", "/invalidUsernameOrPassword", "/js/**", "/css/**", "/img/**", "/static/**", "/resetpassword", "/error") // anyone can see home, the ads pages, and sign up
                 .permitAll();
 
         return http.build();
